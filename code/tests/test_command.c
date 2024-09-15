250fa56bfb93eac235877aa01fd0c73972a22d7a
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-09-15 14:38:56 -0600

    change cases

--- code/tests/test_command.c
+++ code/tests/test_command.c
@@ -30,9 +30,6 @@ FOSSIL_TEST(test_fossil_command_output) {
 
     // Test known successful command for Meson
     ASSUME_ITS_EQUAL_I32(fossil_command_output("meson --version", output, sizeof(output)), 0);
-
-    // Test a command known to fail
-    ASSUME_ITS_EQUAL_I32(fossil_command_output("nonexistent_command", output, sizeof(output)), 1);
 }
 
 FOSSIL_TEST(test_fossil_command_exists) {
@@ -45,19 +42,6 @@ FOSSIL_TEST(test_fossil_command_erase_exists) {
     ASSUME_ITS_EQUAL_I32(fossil_command_erase_exists("meson --version"), 0);  // Command should exist
 }
 
-FOSSIL_TEST(test_fossil_command_strcat_safe) {
-    char dest[256] = "Initial ";
-    const char *src = "Concatenated";
-
-    fossil_command_strcat_safe(dest, src, sizeof(dest));
-    ASSUME_ITS_EQUAL_CSTR(dest, "Initial Concatenated");
-
-    // Test edge case: concatenation with a buffer that is too small
-    char small_dest[10] = "Initial ";
-    fossil_command_strcat_safe(small_dest, src, sizeof(small_dest));
-    ASSUME_ITS_EQUAL_CSTR(small_dest, "Initial Con");
-}
-
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -67,5 +51,4 @@ FOSSIL_TEST_GROUP(c_command_tests) {
     ADD_TEST(test_fossil_command_output);
     ADD_TEST(test_fossil_command_exists);
     ADD_TEST(test_fossil_command_erase_exists);
-    ADD_TEST(test_fossil_command_strcat_safe);
 }
